e9f085984f6e9e9089b517865ff986d4a1ab9431
Sanyam Rawat <san77y1am@gmail.com>
2025-02-15 11:20:51 +0530

    added a background to chats

--- src/components/Chatwindow.jsx
+++ src/components/Chatwindow.jsx
@@ -1,12 +1,22 @@
 import CompanionHeader from "./CompanionHeader.jsx";
 import ChatMessages from "./Chatmessages.jsx";
 import MessageInput from "./MessageInput.jsx";
+import bgPNG from "../assets/bg.png";
 
 export default function ChatWindow() {
   return (
-    <div className="flex-1 flex flex-col">
+    <div className={`flex-1 flex flex-col`}>
       <CompanionHeader />
-      <ChatMessages />
+      <div
+        className="flex h-full overflow-auto"
+        style={{
+          backgroundImage: `url(${bgPNG})`,
+          backgroundSize: "300px",
+          backgroundRepeat: "repeat",
+        }}
+      >
+        <ChatMessages />
+      </div>
       <MessageInput />
     </div>
   );
